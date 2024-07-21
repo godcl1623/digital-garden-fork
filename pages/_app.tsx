506a3635322ee0f06db7d788a288964c444d2183
godcl1623 <godcl1623@gmail.com>
2024-07-21 15:33:48 +0900

    fix: added basic types to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import "../styles/global.css";
-import "../styles/style.css";
-import "../styles/prism.css";
-import PlausibleProvider from "next-plausible";
-export default function App({ Component, pageProps }) {
+import '../styles/global.css';
+import '../styles/style.css';
+import '../styles/prism.css';
+import PlausibleProvider from 'next-plausible';
+import { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="mindstone.tuancao.me">
+    <PlausibleProvider domain='mindstone.tuancao.me'>
       <Component {...pageProps} />
     </PlausibleProvider>
   );
