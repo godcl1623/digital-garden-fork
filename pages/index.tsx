2ba659804daadf66050f79c58c945760ebc991c5
godcl1623 <godcl1623@gmail.com>
2024-07-21 15:39:15 +0900

    fix: fixed main page type errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,24 @@ import {
   getFlattenArray,
   getLocalGraphData,
   constructGraphData,
+  GraphData,
+  ParsedPostContent,
+  ParsedPostDirectoryData,
+  ParsedPostData,
+  GraphRawNodeValue,
 } from '../lib/utils';
 import FolderTree from '../components/FolderTree';
 import dynamic from 'next/dynamic';
 import MDContent from '../components/MDContent';
 import { getTree } from '../lib/postsCache';
+
+interface HomeProps {
+  graphData: GraphData;
+  content: ParsedPostContent;
+  tree: ParsedPostDirectoryData;
+  flattenNodes: (ParsedPostDirectoryData | ParsedPostData)[];
+  backLinks: GraphRawNodeValue[];
+}
 
 // This trick is to dynamically load component that interact with window object (browser only)
 const DynamicGraph = dynamic(() => import('../components/Graph'), {
@@ -16,40 +29,39 @@ const DynamicGraph = dynamic(() => import('../components/Graph'), {
   ssr: false,
 });
 
-export default function Home({ graphData, content, tree, flattenNodes, backLinks }) {
-  // console.log('graph: ', graphData)
+export default function Home({ graphData, content, tree, flattenNodes, backLinks }: HomeProps) {
   return (
     <Layout>
       <div className='container'>
         <nav className='nav-bar'>
           <FolderTree tree={tree} flattenNodes={flattenNodes} />
         </nav>
-        <MDContent content={content} backLinks={backLinks} />
+        <MDContent content={content.data} backLinks={backLinks} />
         <DynamicGraph graph={graphData} />
       </div>
     </Layout>
   );
 }
-const { nodes, edges } = constructGraphData();
 
-export function getStaticProps() {
+export function getStaticProps(): { props: HomeProps } {
+  const { nodes, edges } = constructGraphData();
   const tree = getTree();
-  const contentData = getSinglePost('index');
+  const content = getSinglePost('index');
   const flattenNodes = getFlattenArray(tree);
   const listOfEdges = edges.filter(anEdge => anEdge.target === 'index');
   const internalLinks = listOfEdges
     .map(anEdge => nodes.find(aNode => aNode.slug === anEdge.source))
     .filter(element => element !== undefined);
-  const backLinks = [...new Set(internalLinks)];
+  const backLinks = Array.from(new Set(internalLinks));
 
   const graphData = getLocalGraphData('index');
   return {
     props: {
-      content: contentData.data,
-      tree: tree,
-      flattenNodes: flattenNodes,
-      graphData: graphData,
-      backLinks: backLinks,
+      content,
+      tree,
+      flattenNodes,
+      graphData,
+      backLinks,
     },
   };
 }
